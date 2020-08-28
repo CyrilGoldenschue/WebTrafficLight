36e4388d45c78f382142a5d2e905b0e161857a77
Cyril <cyril.goldenschue@cpnv.ch>
2020-08-28 13:47:38 +0200

    send code

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+$state = "state0";
+$Image = "Etat1.jpg";
+switch ($_GET['fire']){
+
+    case "state0": $state = "state1";
+        $Image = "Etat2.jpg";
+        break;
+    case "state1": $state = "state2";
+        $Image = "Etat3.jpg";
+        break;
+
+    case "state2": $state = "state3";
+        $Image = "Etat4.jpg";
+        break;
+
+    case "state3": $state = "state0";
+        $Image = "Etat1.jpg";
+        break;
+    default: $state = "state1";
+        $Image = "Etat1.jpg";
+        break;
+}
+?>
+<html style="background-color: black">
+<body>
+<div align="center" style="vertical-align: center">
+
+<img src="/img/<?php echo $Image ?>"><br><br>
+    <form action="/index.php" method="get">
+        <button value="<?php echo $state ?>" type="submit" name="fire" id="fire">=></button>
+    </form>
+</div>
+</body>
+</html>
