dcb22447dd44726c916ee9462066ca28ee66add7
Cyril <cyril.goldenschue@cpnv.ch>
2020-09-04 09:30:02 +0200

    Add class in project.

--- index.php
+++ index.php
@@ -1,35 +1,75 @@
 <?php
-$state = "state0";
-$Image = "Etat1.jpg";
-switch ($_GET['fire']){
+/*if(isset($_GET['light'])){
+    switch ($_GET['light']){
 
-    case "state0": $state = "state1";
-        $Image = "Etat1.jpg";
-        break;
-    case "state1": $state = "state2";
-        $Image = "Etat2.jpg";
-        break;
+        case "State0":
+            $Image = $_GET['light'];
+            $state = "State1";
+            break;
+        case "State1":
+            $state = "State2";
+            $Image = $_GET['light'];
+            break;
 
-    case "state2": $state = "state3";
-        $Image = "Etat3.jpg";
-        break;
+        case "State2":
+            $state = "State3";
+            $Image = $_GET['light'];
+            break;
 
-    case "state3": $state = "state0";
-        $Image = "Etat4.jpg";
-        break;
-    default: $state = "state1";
-        $Image = "Etat1.jpg";
-        break;
+        case "State3":
+            $state = "State0";
+            $Image = $_GET['light'];
+            break;
+        default:
+            $state = "State1";
+            $Image = $_GET['light'];
+            break;
+    }
 }
+else{
+    $state = "State1";
+    $Image = "State0";
+}*/
+
 ?>
-<html style="background-color: black">
+<!DOCTYPE html>
+<html lang="fr" style="background-color: black">
+<head>
+    <meta charset="UTF-8">
+    <title>PRW1</title>
+    <link rel="stylesheet" href="src/css/style.css"
+</head>
 <body>
+<?php /*
 <div align="center">
 
-<img src="/img/<?= $Image ?>"><br><br>
+<img src="src/img/State<?= $state ?>.jpg"><br><br>
     <form action="/index.php" method="get">
-        <button value="<?= $state ?>" type="submit" name="fire" id="fire">=></button>
+        <button value="<?= $oldState++ ?>" type="submit" name="light">=></button>
     </form>
 </div>
+ */?>
+
+<?php
+
+require_once('src/class/TrafficLight.php');
+
+$trafficLight = new TrafficLight();
+$animationState = $trafficLight->SetState($_GET["light"]);
+$trafficLight->Animation($_GET["light"]);
+?>
+
+<div id="Rectangle">
+
+    <div class="Circle <?=$trafficLight->red?>"></div>
+
+    <div class="Circle <?=$trafficLight->yellow?>"></div>
+
+    <div class="Circle <?=$trafficLight->green?>"></div>
+
+</div>
+<br/>
+<a class="button" href="<?=$trafficLight->SetState($_GET["light"])?>">=></a
+
 </body>
 </html>
---
+++ src/class/TrafficLight.php
@@ -0,0 +1,48 @@
+<?php
+
+
+class TrafficLight
+{
+    public $red;
+    public $yellow;
+    public $green;
+
+    public function SetState($state = 0){
+
+        $nextState = ($state +1) % 4;
+
+        return "/?light=".$nextState;
+    }
+
+    public function Animation($state){
+
+        switch($state)
+        {
+            case 0:
+                $this -> red    = "RedCircle";
+                $this -> yellow= "ExtinctCircle";
+                $this -> green  = "ExtinctCircle";
+                break;
+            case 1:
+                $this -> red    = "RedCircle";
+                $this -> yellow = "OrangeCircle";
+                $this -> green  = "ExtinctCircle";
+                break;
+            case 2:
+                $this -> red    = "ExtinctCircle";
+                $this -> yellow = "ExtinctCircle";
+                $this -> green  = "GreenCircle";
+                break;
+            case 3:
+                $this -> red    = "ExtinctCircle";
+                $this -> yellow = "OrangeCircle";
+                $this -> green  = "ExtinctCircle";
+                break;
+            default:
+                $this -> red    = "ExtinctCircle";
+                $this -> yellow = "ExtinctCircle";
+                $this -> green  = "ExtinctCircle";
+                break;
+        }
+    }
+}
