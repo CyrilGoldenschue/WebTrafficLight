b473a48d5d9f4e395f091e0068d0b5db548566b1
Cyril <cyril.goldenschue@cpnv.ch>
2020-09-04 09:31:32 +0200

    delete code in commentary

--- index.php
+++ index.php
@@ -1,35 +1,9 @@
 <?php
-/*if(isset($_GET['light'])){
-    switch ($_GET['light']){
+require_once('src/class/TrafficLight.php');
 
-        case "State0":
-            $Image = $_GET['light'];
-            $state = "State1";
-            break;
-        case "State1":
-            $state = "State2";
-            $Image = $_GET['light'];
-            break;
-
-        case "State2":
-            $state = "State3";
-            $Image = $_GET['light'];
-            break;
-
-        case "State3":
-            $state = "State0";
-            $Image = $_GET['light'];
-            break;
-        default:
-            $state = "State1";
-            $Image = $_GET['light'];
-            break;
-    }
-}
-else{
-    $state = "State1";
-    $Image = "State0";
-}*/
+$trafficLight = new TrafficLight();
+$animationState = $trafficLight->SetState($_GET["light"]);
+$trafficLight->Animation($_GET["light"]);
 
 ?>
 <!DOCTYPE html>
@@ -40,25 +14,6 @@ else{
     <link rel="stylesheet" href="src/css/style.css"
 </head>
 <body>
-<?php /*
-<div align="center">
-
-<img src="src/img/State<?= $state ?>.jpg"><br><br>
-    <form action="/index.php" method="get">
-        <button value="<?= $oldState++ ?>" type="submit" name="light">=></button>
-    </form>
-</div>
- */?>
-
-<?php
-
-require_once('src/class/TrafficLight.php');
-
-$trafficLight = new TrafficLight();
-$animationState = $trafficLight->SetState($_GET["light"]);
-$trafficLight->Animation($_GET["light"]);
-?>
-
 <div id="Rectangle">
 
     <div class="Circle <?=$trafficLight->red?>"></div>
