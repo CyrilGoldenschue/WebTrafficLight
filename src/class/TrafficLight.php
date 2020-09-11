8e27ba673fe23a4770051fa90e3ffc63ea080572
Cyril <cyril.goldenschue@cpnv.ch>
2020-09-11 10:06:57 +0200

    remove of redundancy

--- src/class/TrafficLight.php
+++ src/class/TrafficLight.php
@@ -13,26 +13,26 @@ class TrafficLight
 
             switch($state)
             {
-                case 0:
+                case 0: //stop
                     $this -> red    = true;
                     $this -> pause = true;
-                    $nextState = ($state +1) % 4;
+                    $nextState = $this -> Next($state);
                     break;
-                case 1:
+                case 1: //set
                     $this -> red    = true;
                     $this -> yellow = true;
-                    $nextState = ($state +1) % 4;
+                    $nextState = $this -> Next($state);
                     break;
-                case 2:
+                case 2: //go
                     $this -> green  = true;
                     $this -> pause = true;
-                    $nextState = ($state +1) % 4;
+                    $nextState = $this -> Next($state);
                     break;
-                case 3:
+                case 3: //slow
                     $this -> yellow = true;
-                    $nextState = ($state +1) % 4;
+                    $nextState = $this -> Next($state);
                     break;
-                case 4:
+                case 4: //pause
                     $this -> yellow = true;
                     $this -> blink = true;
                     $nextState = 4;
@@ -42,10 +42,14 @@ class TrafficLight
             }
 
 
-
         return $nextState;
     }
 
+    function Next($state=0){
+
+        $nextState = ($state +1) % 4;
+        return $nextState;
+    }
 
 
     function GetRedCircle(){
